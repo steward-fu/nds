3917dd959a427b3b78bba4bf6084430a519cd8ca
steward-fu <steward.fu@gmail.com>
2023-10-26 10:47:09 -0400

    v1.1

--- libasound/main.c
+++ libasound/main.c
@@ -19,15 +19,16 @@
 #include "mi_common_datatype.h"
 #include "mi_ao.h"
 
-static MI_AUDIO_Attr_t stSetAttr;
-static MI_AUDIO_Attr_t stGetAttr;
 static MI_AO_CHN AoChn = 0;
 static MI_AUDIO_DEV AoDevId = 0;
+static MI_AUDIO_Attr_t stSetAttr = {0};
+static MI_AUDIO_Attr_t stGetAttr = {0};
 
 static int cur_volume = 0;
 static int pcm_ready = 0;
 static int pcm_buf_len = 0;
 static uint8_t *pcm_buf = NULL;
+static uint8_t *pcm_dummy = NULL;
 static struct json_object *jfile = NULL;
 
 #define JSON_FILE           "/appconfigs/system.json"
@@ -36,36 +37,13 @@ static struct json_object *jfile = NULL;
 #define FREQ                44100
 #define CHANNELS            2
 #define SAMPLES             8192
-#define QUEUE_SIZE          (512 * 1024)
-
-/*
-    DraStic:
-        call snd_pcm_open
-        call snd_pcm_hw_params_malloc
-        call snd_pcm_hw_params_any
-        call snd_pcm_hw_params_set_access
-        call snd_pcm_hw_params_set_format
-        call snd_pcm_hw_params_set_rate_near
-        call snd_pcm_hw_params_set_channels
-        call snd_pcm_hw_params_set_buffer_size_near
-        call snd_pcm_hw_params_set_period_size_near
-            ALSA output: requested rate of 44100, got 44100.
-            ALSA output: requested buffer size of 8192, got 8192.
-            ALSA output: requested period size of 2048, got 2048.
-        call snd_pcm_hw_params
-        call snd_pcm_hw_params_free
-        call snd_pcm_sw_params_malloc
-        call snd_pcm_sw_params_current
-        call snd_pcm_sw_params
-        call snd_pcm_sw_params_free
-        call snd_pcm_prepare
-        call snd_pcm_start
-*/
+#define QUEUE_SIZE          (2 * 1024 * 1024)
 
 typedef struct {
-    size_t size;
+    int size;
+    int read;
+    int write;
     uint8_t *buffer;
-    size_t head, tail;
     pthread_mutex_t lock;
 } queue_t;
 
@@ -166,77 +144,129 @@ static void queue_init(queue_t *q, size_t s)
 {
     q->buffer = (uint8_t *)malloc(s);
     q->size = s;
-    q->head = q->tail = 0;
+    q->read = q->write = 0;
     pthread_mutex_init(&q->lock, NULL);
 }
 
 static void queue_destroy(queue_t *q)
 {
-    free(q->buffer);
+    if (q->buffer) {
+        free(q->buffer);
+    }
     pthread_mutex_destroy(&q->lock);
+}
+
+static int queue_size_for_read(queue_t *q)
+{
+    if (q->read == q->write) {
+        return 0;
+    }
+    else if(q->read < q->write){
+        return q->write - q->read;
+    }
+    return (QUEUE_SIZE - q->read) + q->write;
+}
+
+static int queue_size_for_write(queue_t *q)
+{
+    if (q->write == q->read) {
+        return QUEUE_SIZE;
+    }
+    else if(q->write < q->read){
+        return q->read - q->write;
+    }
+    return (QUEUE_SIZE - q->write) + q->read;
 }
 
 static int queue_put(queue_t *q, uint8_t *buffer, size_t size)
 {
+    int r = 0, tmp = 0, avai = 0;
+
     pthread_mutex_lock(&q->lock);
-    if (((q->tail + sizeof(size)) % q->size) == q->head) {
-        //printf("%s, queue full\n", __func__);
+    avai = queue_size_for_write(q);
+    if (size > avai) {
+        size = avai;
     }
-    else {
-        memcpy(&q->buffer[q->tail], buffer, size);
-        q->tail += size;
-        q->tail %= q->size;
+    r = size;
+
+    if (size > 0) {
+        if ((q->write >= q->read) && ((q->write + size) > QUEUE_SIZE)) {
+            tmp = QUEUE_SIZE - q->write;
+            size-= tmp;
+            memcpy(&q->buffer[q->write], buffer, tmp);
+            memcpy(q->buffer, &buffer[tmp], size);
+            q->write = size;
+        }
+        else {
+            memcpy(&q->buffer[q->write], buffer, size);
+            q->write += size;
+        }
     }
     pthread_mutex_unlock(&q->lock);
+    return r;
 }
 
 static size_t queue_get(queue_t *q, uint8_t *buffer, size_t max)
 {
-    int len = 0;
+    int r = 0, tmp = 0, avai = 0, size = max;
 
     pthread_mutex_lock(&q->lock);
-    if ((q->tail - q->head) > 0) {
-        len = (q->tail - q->head);
-        len = len > max ? max : len;
-        memcpy(buffer, &q->buffer[q->head], len);
-        q->head += len;
-        q->head %= q->size;
+    avai = queue_size_for_read(q);
+    if (size > avai) {
+        size = avai;
+    }
+    r = size;
+
+    if (size > 0) {
+        if ((q->read > q->write) && (q->read + size) > QUEUE_SIZE) {
+            tmp = QUEUE_SIZE - q->read;
+            size-= tmp;
+            memcpy(buffer, &q->buffer[q->read], tmp);
+            memcpy(&buffer[tmp], q->buffer, size);
+            q->read = size;
+        }
+        else {
+            memcpy(buffer, &q->buffer[q->read], size);
+            q->read+= size;
+        }
     }
     pthread_mutex_unlock(&q->lock);
-    return len;
+    return r;
 }
 
 static void *audio_handler(void *threadid)
 {
-    int r = 0, len = pcm_buf_len;
+    MI_S32 s32RetSendStatus = 0;
+    MI_AUDIO_Frame_t aoTestFrame;
+    int r = 0, len = pcm_buf_len, use_dummy = 0;
 
     while (pcm_ready) {
+        use_dummy = 1;
         r = queue_get(&queue, pcm_buf, len);
         if (r > 0) {
             len-= r;
             if (len == 0) {
-                MI_S32 s32RetSendStatus = 0;
-                MI_AUDIO_Frame_t aoTestFrame;
-        
+                use_dummy = 0;
                 len = pcm_buf_len;
-                aoTestFrame.eBitwidth = stGetAttr.eBitwidth;
-                aoTestFrame.eSoundmode = stGetAttr.eSoundmode;
-                aoTestFrame.u32Len = len;
-                aoTestFrame.apVirAddr[0] = pcm_buf;
-                aoTestFrame.apVirAddr[1] = NULL;
-                do {
-                    s32RetSendStatus = MI_AO_SendFrame(AoDevId, AoChn, &aoTestFrame, 1);
-                    usleep(((stSetAttr.u32PtNumPerFrm * 1000) / stSetAttr.eSamplerate - 10) * 1000);
-                } while(s32RetSendStatus == MI_AO_ERR_NOBUF);
             }
         }
+        
+        aoTestFrame.eBitwidth = stGetAttr.eBitwidth;
+        aoTestFrame.eSoundmode = stGetAttr.eSoundmode;
+        aoTestFrame.u32Len = pcm_buf_len;
+        aoTestFrame.apVirAddr[0] = use_dummy ? pcm_dummy : pcm_buf;
+        aoTestFrame.apVirAddr[1] = NULL;
+        do {
+            s32RetSendStatus = MI_AO_SendFrame(AoDevId, AoChn, &aoTestFrame, 1);
+            usleep(((stSetAttr.u32PtNumPerFrm * 1000) / stSetAttr.eSamplerate - 10) * 1000);
+        } while(s32RetSendStatus == MI_AO_ERR_NOBUF);
     }
     pthread_exit(NULL);
 }
 
 snd_pcm_sframes_t snd_pcm_avail(snd_pcm_t *pcm)
 {
-    return pcm_buf_len;
+    return 0; //pcm_buf_len;
 }
 
 int snd_pcm_hw_params(snd_pcm_t *pcm, snd_pcm_hw_params_t *params)
@@ -323,11 +353,23 @@ int snd_pcm_start(snd_pcm_t *pcm)
     MI_SYS_ChnPort_t stAoChn0OutputPort0;
     
     queue_init(&queue, (size_t)QUEUE_SIZE);
-    pcm_buf_len = SAMPLES * 2 * CHANNELS;
-    pcm_buf = malloc(pcm_buf_len);
-    if(pcm_buf == NULL) {
+    if (queue.buffer == NULL) {
         return -1;
     }
+    memset(queue.buffer, 0, QUEUE_SIZE);
+
+    pcm_buf_len = SAMPLES * 2 * CHANNELS;
+    pcm_buf = malloc(pcm_buf_len);
+    if (pcm_buf == NULL) {
+        return -1;
+    }
+    memset(pcm_buf, 0, pcm_buf_len);
+
+    pcm_dummy = malloc(pcm_buf_len);
+    if (pcm_dummy == NULL) {
+        return -1;
+    }
+    memset(pcm_dummy, 0, pcm_buf_len);
 
     jfile = json_object_from_file(JSON_FILE);
     if (jfile != NULL) {
@@ -388,11 +430,11 @@ int snd_pcm_close(snd_pcm_t *pcm)
 {
     void *ret = NULL;
 
-
     pcm_ready = 0;
     pthread_join(thread, &ret);
-    queue_destroy(&queue);
     free(pcm_buf);
+    free(pcm_dummy);
+    queue_destroy(&queue);
     MI_AO_DisableChn(AoDevId, AoChn);
     MI_AO_Disable(AoDevId);
     return 0;
