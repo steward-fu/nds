7886be4e0aa1888e5b8d4d6c0277155e96ca4bdb
steward-fu <steward.fu@gmail.com>
2023-12-16 21:39:57 -0500

    Added musl and glibc supports for FunKey-S

--- funkeys/opk/launch.sh
+++ funkeys/opk/launch.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 echo "$1" > /mnt/drastic/rom.txt
-mount mmiyoo.squashfs mnt
-mount --bind /sys mnt/sys
-mount --bind /dev mnt/dev
-mount --bind /proc mnt/proc
-mount --bind /mnt/drastic mnt/mnt/emu
-mount --bind -o iocharset=utf8 /mnt/NDS mnt/mnt/NDS
-chroot mnt /bin/sh -c /mnt/emu/launch.sh
-umount mnt/mnt/emu
-umount mnt/mnt/NDS
-umount mnt/proc
-umount mnt/sys
-umount mnt/dev
-umount mnt
+
+if [ -f "/lib/ld-linux-armhf.so.3" ]; then
+    /mnt/drastic/launch.sh
+else
+    mount mmiyoo.squashfs mnt
+    mount --bind /sys mnt/sys
+    mount --bind /dev mnt/dev
+    mount --bind /proc mnt/proc
+    mount --bind /mnt/drastic mnt/mnt/emu
+    mount --bind -o iocharset=utf8 /mnt/NDS mnt/mnt/NDS
+    chroot mnt /bin/sh -c /mnt/emu/launch.sh
+    umount mnt/mnt/emu
+    umount mnt/mnt/NDS
+    umount mnt/proc
+    umount mnt/sys
+    umount mnt/dev
+    umount mnt
+fi
