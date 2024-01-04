54c9c83ebc796374d8eb28efb758308601e273f4
steward-fu <g9313716@yuntech.edu.tw>
2024-01-04 22:09:51 +0800

    Used the system files for Pandora

--- assets/pandora/launch.sh
+++ assets/pandora/launch.sh
@@ -8,6 +8,21 @@ export SDL_VIDEODRIVER=mmiyoo
 export SDL_AUDIODRIVER=mmiyoo
 export EGL_VIDEODRIVER=mmiyoo
 
+if [ -f "libs/libpng16.so.16" ]; then
+    mv libs/libpng16.so.16 libs/__libpng16.so.16
+    mv libs/libz.so.1 libs/__libz.so.1
+    mv libs/libSDL2_image-2.0.so.0 libs/__libSDL2_image-2.0.so.0
+    mv libs/libSDL2_ttf-2.0.so.0 libs/__libSDL2_ttf-2.0.so.0
+fi
+
 cd $mydir
 ./drastic "$1"
 sync
+./restore
+
+if [ -f "libs/__libpng16.so.16" ]; then
+    mv libs/__libpng16.so.16 libs/libpng16.so.16
+    mv libs/__libz.so.1 libs/libz.so.1
+    mv libs/__libSDL2_image-2.0.so.0 libs/libSDL2_image-2.0.so.0
+    mv libs/__libSDL2_ttf-2.0.so.0 libs/libSDL2_ttf-2.0.so.0
+fi
