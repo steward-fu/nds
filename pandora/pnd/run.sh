4d9096f19bd0247186c2b2bd9a3667902236a6c8
steward-fu <g9313716@yuntech.edu.tw>
2024-01-12 14:25:18 +0800

    Removed pandora build

--- pandora/pnd/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-mydir=`dirname "$0"`
-
-cd $mydir
-`./launcher`
-echo "$?"
-#op_runfbapp ./drastic /media/Linux/home/steward/Data/nds/pandora/pnd/1.nds
-#ofbset -fb /dev/fb1 -pos 0 0 -size 0 0 -mem 0 -en 0
-#sync
