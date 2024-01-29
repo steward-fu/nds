96ee8d373fbd31db27b13a1846277d31f4572552
steward-fu <steward.fu@gmail.com>
2024-01-29 09:51:42 -0500

    Trimui: fixed L2R2 key issue.

--- assets/trimui/launch.sh
+++ assets/trimui/launch.sh
@@ -6,9 +6,11 @@ export PATH=$mydir:$PATH
 export LD_LIBRARY_PATH=$mydir/libs:$LD_LIBRARY_PATH
 
 sv=`cat /proc/sys/vm/swappiness`
+com=`cat /sys/module/gpio_keys_polled/parameters/l2r2combokey`
 
 # 60 by default
 echo 10 > /proc/sys/vm/swappiness
+echo 0 > /sys/module/gpio_keys_polled/parameters/l2r2combokey
 
 cd $mydir
 if [ ! -f "/tmp/.show_hotkeys" ]; then
@@ -25,3 +27,4 @@ cd $mydir
 sync
 
 echo $sv > /proc/sys/vm/swappiness
+echo $com > /sys/module/gpio_keys_polled/parameters/l2r2combokey
