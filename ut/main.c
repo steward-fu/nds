steward-fu/nds_miyoo

Refactoring code.

ut/main.c
@@ -1,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "unity_fixture.h"
+
+static void runAllTests(void)
+{
+    RUN_TEST_GROUP(common_log);
+    RUN_TEST_GROUP(common_cfg);
+    RUN_TEST_GROUP(common_file);
+    RUN_TEST_GROUP(alsa_snd);
+    RUN_TEST_GROUP(detour_hook);
+    RUN_TEST_GROUP(sdl2_audio_mmiyoo);
+    RUN_TEST_GROUP(sdl2_video_mmiyoo);
+    RUN_TEST_GROUP(sdl2_event_mmiyoo);
+    RUN_TEST_GROUP(sdl2_render_mmiyoo);
+}
+
+int main(int argc, const char **argv)
+{
+    printf("\n");
+    return UnityMain(argc, argv, runAllTests);
+}
+

steward-fu/nds

ut/main.c
@@ -1,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "unity_fixture.h"
+
+static void runAllTests(void)
+{
+    RUN_TEST_GROUP(alsa_snd);
+
+    RUN_TEST_GROUP(common_log);
+    RUN_TEST_GROUP(common_cfg);
+    RUN_TEST_GROUP(common_file);
+
+    RUN_TEST_GROUP(detour_hook);
+
+    RUN_TEST_GROUP(sdl2_joy);
+    RUN_TEST_GROUP(sdl2_menu);
+    RUN_TEST_GROUP(sdl2_video);
+    RUN_TEST_GROUP(sdl2_event);
+    RUN_TEST_GROUP(sdl2_render);
+}
+
+int main(int argc, const char **argv)
+{
+    printf("\n");
+    return UnityMain(argc, argv, runAllTests);
+}
+