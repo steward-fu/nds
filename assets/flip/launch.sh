caa672120a9e2a4e3e2e4e72f5fbc7bc0ac46741
steward-fu <steward.fu@gmail.com>
2025-04-27 20:15:17 +0800

    Fixed characters issue.

--- assets/flip/launch.sh
+++ assets/flip/launch.sh
@@ -1,21 +1,27 @@
 #!/bin/sh
 MYDIR=`dirname "$0"`
+PORT32=`dirname "$0"`/../PORT32
+IMG=miyoo355_rootfs_32.img
 
 cd $MYDIR
 
-if [ ! -f miyoo355_rootfs_32.img ]; then
+if [ ! -f $IMG ]; then
+if [ ! -f $PORT32/$IMG ]; then
     exit
 fi
+IMG=$PORT32/$IMG
+fi
+
 
 mkdir -p mnt
-mount -t squashfs miyoo355_rootfs_32.img mnt
+mount -t squashfs $IMG mnt
 mount --bind /sys mnt/sys
 mount --bind /dev mnt/dev
 mount --bind /proc mnt/proc
 mount --bind /var/run mnt/var/run
 mount --bind /mnt/sdcard mnt/sdcard
 
-chroot mnt /bin/sh -c "cd /sdcard/Emu/drastic && ./run.sh $1"
+chroot mnt /bin/sh -c "cd /sdcard/Emu/drastic && ./run.sh \"$1\""
 
 umount mnt/sdcard
 umount mnt/var/run
