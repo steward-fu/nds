1d26208dd139ba2c735c4cbdeec67b386354deb5
steward-fu <steward.fu@gmail.com>
2025-05-26 22:31:30 +0800

    Added cpu setting for XT897.

--- assets/xt897/launch.sh
+++ assets/xt897/launch.sh
@@ -6,6 +6,10 @@ export PATH=$MYDIR:$PATH
 export SDL_VIDEODRIVER=NDS
 export LD_LIBRARY_PATH=lib:/usr/local/lib:$LD_LIBRARY_PATH
 
+echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+
 cd $MYDIR
 ./drastic
 sync
+
+echo interactive > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
