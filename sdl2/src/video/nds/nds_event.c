3ff65a9d2f02a3a5f05e8ba5976792523a8e3b75
steward-fu <steward.fu@gmail.com>
2025-06-21 06:50:30 +0800

    Fixed M+Y issue.

--- sdl2/src/video/nds/nds_event.c
+++ sdl2/src/video/nds/nds_event.c
@@ -840,7 +840,7 @@ static int handle_hotkey(void)
     if (hit_hotkey(KEY_BIT_Y)) {
         if (check_hotkey) {
             if (myevent.mode == NDS_KEY_MODE) {
-                if (!myvideo.layout.overlay.max &&
+                if ((myconfig.layout.mode.sel != LAYOUT_MODE_OV) &&
                     (myconfig.layout.mode.sel != LAYOUT_MODE_T0) &&
                     (myconfig.layout.mode.sel != LAYOUT_MODE_T1) &&
                     (myconfig.layout.mode.sel != LAYOUT_MODE_T3))
